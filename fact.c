f7cad7082945661505408f25ed349343093330d3
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-02-18 16:09:10 +0700

    update fact

--- fact.c
+++ fact.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
 int main(){
 	int k;
+	int a=2;
+	scanf("%d");
+    for(;k!=0;)
+	{
+		if(k%a==0)
+		{
+			printf("%d",a);
+			k=k/a;
+		}
+		else
+		{
+			a++;
+		}
+	}
+	if(k==0)
+	{
+		printf("0");
+	}
 	return 0;
 }
