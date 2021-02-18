19f1c121b84f1a33695e960ab7d2875bb720e563
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-02-18 14:49:38 +0700

    Update pet.cpp

--- pet.cpp
+++ pet.cpp
@@ -1,10 +1,18 @@
 #include<stdio.h>
 int main(){
-	int point[5][4];
+	int point[4[5];
 	int i,j;
-	for(j=0;j<4;j++)
+	for(j=0;j<5;j++)
 	{
-		for(i=0)
+		for(i=0;i<4;i++)
+		{
+			scanf("%d",&point[i][l]);
+		}
 	}
+	int fst,sec,trd,fth,fith;
+	fst=point[0][0]+point[1][0]+point[2][0]+point[3][0];
+	sec=point[0][1]+point[1][1]+point[2][1]+point[3][1];
+	ted=point[0][2]+point[1][2]+point[2][2]+point[3][2];
+	f
 	return 0;
 }
