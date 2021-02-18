a059aeed2133ada3f9bd5b8e6b813bb6be153e83
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-02-18 16:28:34 +0700

    update fact

--- fact.c
+++ fact.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
-#include<math.h>
+
 int main(){
 	int k;
 	int a=2,i=3;
 	scanf("%d",&k);
-	int kk=k;
     while (k%2==0) 
     { 
         printf("%d\n",a); 
         k = k/2; 
     } 
-	for(i=3;i<kk;i+=2)
+	for(i=3;i<=10;i+=2)
     {
     	while(k%i==0)
 		{
